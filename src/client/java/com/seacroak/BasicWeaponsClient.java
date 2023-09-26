0769e8d2c1489003b93a51d0ed1a12e6bd94eac6
Khazoda <khazoda@gmail.com>
2023-09-26 01:28:54 +0100

    Runnable state
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- src/client/java/com/seacroak/BasicWeaponsClient.java
+++ src/client/java/com/seacroak/BasicWeaponsClient.java
@@ -2,7 +2,7 @@ package com.seacroak;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class BasicWeaponsClient implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
     // This entrypoint is suitable for setting up client-specific logic, such as rendering.
