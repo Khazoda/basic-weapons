3a0d30ee4fca798409aeecddb7d1e6e0ce809575
Khazoda <khazoda@gmail.com>
2023-09-27 16:45:35 +0100

    + Hammer, club effects
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/item/ClubItem.java
+++ src/main/java/com/seacroak/basicweapons/item/ClubItem.java
@@ -1,9 +1,23 @@
 package com.seacroak.basicweapons.item;
 
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
 
 public class ClubItem extends BasicWeaponItem {
   public ClubItem(ToolMaterial tier, int damage, float attackSpeed, Settings properties) {
     super(tier, damage, attackSpeed, properties);
+  }
+
+  @Override
+  public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+    target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 10));
+    stack.damage(1, attacker, (e) -> {
+      e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
+    });
+    return true;
   }
 }
--- src/main/java/com/seacroak/basicweapons/item/HammerItem.java
+++ src/main/java/com/seacroak/basicweapons/item/HammerItem.java
@@ -1,9 +1,21 @@
 package com.seacroak.basicweapons.item;
 
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
 
 public class HammerItem extends BasicWeaponItem {
   public HammerItem(ToolMaterial tier, int damage, float attackSpeed, Settings properties) {
     super(tier, damage, attackSpeed, properties);
+  }
+
+  @Override
+  public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+    target.addVelocity(0, 0.35, 0);
+    stack.damage(1, attacker, (e) -> {
+      e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
+    });
+    return true;
   }
 }
