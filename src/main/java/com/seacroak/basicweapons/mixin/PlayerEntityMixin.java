9c9366637c0da5306d8863b2f0a2aa91f6171060
Khazoda <khazoda@gmail.com>
2023-10-16 13:19:42 +0100

    Removed sweeping mixin strategy, moving to custom enchantments instead
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/mixin/PlayerEntityMixin.java
+++ src/main/java/com/seacroak/basicweapons/mixin/PlayerEntityMixin.java
@@ -1,10 +1,8 @@
 package com.seacroak.basicweapons.mixin;
 
-import com.seacroak.basicweapons.item.BasicWeaponItem;
 import com.seacroak.basicweapons.mixinutils.PlayerEntityAccessor;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -26,20 +24,8 @@ public abstract class PlayerEntityMixin implements PlayerEntityAccessor {
     return this.attackCooldownProgress;
   }
 
-  @Unique
-  protected boolean bl4 = false;
-  @Unique
-  protected ItemStack itemStack;
-
   @Inject(method = "attack", at = @At("HEAD"))
   private void retrieveCooldownEarly(Entity target, CallbackInfo ci) {
     attackCooldownProgress = this.getAttackCooldownProgress(0.5f);
-  }
-
-  @Inject(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", shift = At.Shift.AFTER))
-  private void preventSweeping(Entity target, CallbackInfo ci) {
-    if (itemStack.getItem() instanceof BasicWeaponItem) {
-      bl4 = false;
-    }
   }
 }
