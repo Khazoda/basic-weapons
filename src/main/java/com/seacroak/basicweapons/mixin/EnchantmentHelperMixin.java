8c6fe81e1fd7463f79e07d2958b631426fa7bd55
Khazoda <khazoda@gmail.com>
2023-10-18 21:28:41 +0100

    Mixin conflict with Spellpower fixed. Walljump mod conflict still exists
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/mixin/EnchantmentHelperMixin.java
+++ src/main/java/com/seacroak/basicweapons/mixin/EnchantmentHelperMixin.java
@@ -1,37 +1,35 @@
 package com.seacroak.basicweapons.mixin;
 
-import com.seacroak.basicweapons.item.*;
+import com.seacroak.basicweapons.item.BasicWeaponItem;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.item.Item;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 import static net.minecraft.enchantment.Enchantments.*;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
-  @Unique
-  private static Enchantment currentEnchantment;
 
-  @Redirect(method = "getPossibleEntries", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/Enchantment;isAvailableForRandomSelection()Z"))
-  private static boolean isAvailableForRandomSelection(Enchantment enchantment) {
-    currentEnchantment = enchantment;
+  @Inject(method = "getPossibleEntries", at = @At("TAIL"))
+  private static void applyEnchantmentsToBasicWeapons(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
 
-    return enchantment.isAvailableForRandomSelection();
-  }
+    Enchantment[] bwValidEnchantments = {SHARPNESS, SMITE, BANE_OF_ARTHROPODS, KNOCKBACK, FIRE_ASPECT, LOOTING};
+    if (stack.getItem() instanceof BasicWeaponItem) {
+      var currentEntries = cir.getReturnValue();
 
-  @Redirect(method = "getPossibleEntries", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentTarget;isAcceptableItem(Lnet/minecraft/item/Item;)Z"))
-  private static boolean isAcceptableItem(EnchantmentTarget enchantmentTarget, Item item) {
-    ItemStack stack = new ItemStack(item);
-
-    if (item instanceof ClubItem || item instanceof DaggerItem || item instanceof HammerItem || item instanceof QuarterstaffItem || item instanceof SpearItem)
-      return currentEnchantment.isAcceptableItem(stack) || currentEnchantment == SHARPNESS || currentEnchantment == SMITE || currentEnchantment == BANE_OF_ARTHROPODS || currentEnchantment == KNOCKBACK || currentEnchantment == FIRE_ASPECT || currentEnchantment == LOOTING;
-
-    return enchantmentTarget.isAcceptableItem(item);
+      for (Enchantment enchantment : bwValidEnchantments) {
+        for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
+          if (power < enchantment.getMinPower(i) || power > enchantment.getMaxPower(i)) continue;
+          currentEntries.add(new EnchantmentLevelEntry(enchantment, i));
+        }
+      }
+    }
   }
 }
