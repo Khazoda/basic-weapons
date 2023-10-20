b095c00d6cc889e5f20ced401b950e47d7582056
Khazoda <khazoda@gmail.com>
2023-10-20 18:48:44 +0100

    Written sneaky movespeed sweeping mixins
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/mixin/PlayerEntityMixin.java
+++ src/main/java/com/seacroak/basicweapons/mixin/PlayerEntityMixin.java
@@ -1,13 +1,17 @@
 package com.seacroak.basicweapons.mixin;
 
+import com.seacroak.basicweapons.item.BasicWeaponItem;
 import com.seacroak.basicweapons.mixinutils.PlayerEntityAccessor;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Hand;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
@@ -27,5 +31,34 @@ public abstract class PlayerEntityMixin implements PlayerEntityAccessor {
   @Inject(method = "attack", at = @At("HEAD"))
   private void retrieveCooldownEarly(Entity target, CallbackInfo ci) {
     attackCooldownProgress = this.getAttackCooldownProgress(0.5f);
+  }
+
+
+//  @ModifyVariable(method = "attack", at = @At("STORE"), ordinal = 15)
+//  public double modifyAttackSpeed(double x) {
+//    PlayerEntity pe = (PlayerEntity) (Object) this;
+//    return pe.getMovementSpeed() +x + 0.1f;
+//  }
+
+  @Redirect(method = "attack", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerEntity;horizontalSpeed:F", opcode = Opcodes.GETFIELD))
+  private float alterHorizontalSpeedToSneakilyPreventSweeping(PlayerEntity instance) {
+    if (instance.getStackInHand(Hand.MAIN_HAND).getItem() instanceof BasicWeaponItem)
+      return 10f;
+    return instance.horizontalSpeed;
+  }
+
+  @Redirect(method = "attack", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerEntity;prevHorizontalSpeed:F", opcode = Opcodes.GETFIELD))
+  private float alterPreviousHorizontalSpeedToSneakilyPreventSweeping(PlayerEntity instance) {
+    if (instance.getStackInHand(Hand.MAIN_HAND).getItem() instanceof BasicWeaponItem)
+      return 0f;
+    return instance.prevHorizontalSpeed;
+  }
+
+  @Inject(method = "tick()V", at = @At("HEAD"))
+  private void amogus(CallbackInfo ci) {
+    PlayerEntity t = ((PlayerEntity) (Object) this);
+    float f = t.horizontalSpeed;
+    float g = t.prevHorizontalSpeed;
+    System.out.println("S: " + f + " P: " + g + " T: " + (f - g));
   }
 }
