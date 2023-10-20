1fcbf1396bab1abd0df5758f1be53bf5cedca063
Khazoda <khazoda@gmail.com>
2023-10-20 15:45:27 +0100

    Testing removing enchantments instead of adding (for compatibility)
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/mixin/EnchantmentHelperMixin.java
+++ src/main/java/com/seacroak/basicweapons/mixin/EnchantmentHelperMixin.java
@@ -1,7 +1,6 @@
 package com.seacroak.basicweapons.mixin;
 
-import com.seacroak.basicweapons.item.BasicWeaponItem;
-import net.minecraft.enchantment.Enchantment;
+import com.seacroak.basicweapons.item.*;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.item.ItemStack;
@@ -12,24 +11,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
-import static net.minecraft.enchantment.Enchantments.*;
+import static net.minecraft.enchantment.Enchantments.SWEEPING;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
 
   @Inject(method = "getPossibleEntries", at = @At("TAIL"))
-  private static void applyEnchantmentsToBasicWeapons(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
-
-    Enchantment[] bwValidEnchantments = {SHARPNESS, SMITE, BANE_OF_ARTHROPODS, KNOCKBACK, FIRE_ASPECT, LOOTING};
+  private static void blacklistEnchantments(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
     if (stack.getItem() instanceof BasicWeaponItem) {
       var currentEntries = cir.getReturnValue();
-
-      for (Enchantment enchantment : bwValidEnchantments) {
-        for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
-          if (power < enchantment.getMinPower(i) || power > enchantment.getMaxPower(i)) continue;
-          currentEntries.add(new EnchantmentLevelEntry(enchantment, i));
-        }
+      if (stack.getItem() instanceof ClubItem) {
+        currentEntries.removeIf(entry -> entry.enchantment.equals(SWEEPING));
       }
+      if (stack.getItem() instanceof DaggerItem) {
+        currentEntries.removeIf(entry -> entry.enchantment.equals(SWEEPING));
+      }
+      if (stack.getItem() instanceof HammerItem) {
+        currentEntries.removeIf(entry -> entry.enchantment.equals(SWEEPING));
+      }
+      if (stack.getItem() instanceof SpearItem) {
+        currentEntries.removeIf(entry -> entry.enchantment.equals(SWEEPING));
+      }
+      /* if (stack.getItem() instanceof QuarterstaffItem) {} */
+
     }
+//    Enchantment[] bwValidEnchantments = {SHARPNESS, SMITE, BANE_OF_ARTHROPODS, KNOCKBACK, FIRE_ASPECT, LOOTING};
+//    if (stack.getItem() instanceof BasicWeaponItem) {
+//      var currentEntries = cir.getReturnValue();
+//
+//      for (Enchantment enchantment : bwValidEnchantments) {
+//        for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
+//          if (power < enchantment.getMinPower(i) || power > enchantment.getMaxPower(i)) continue;
+//          currentEntries.add(new EnchantmentLevelEntry(enchantment, i));
+//        }
+//      }
+//    }
   }
 }
