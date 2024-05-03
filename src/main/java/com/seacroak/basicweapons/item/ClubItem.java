f3ba996fc774595f4313c3af5b4376e42aa34f11
Khazoda <khazoda@gmail.com>
2024-05-03 14:48:44 +0100

    Reimplemented club and hammer custom behaviour
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/item/ClubItem.java
+++ src/main/java/com/seacroak/basicweapons/item/ClubItem.java
@@ -1,7 +1,14 @@
 package com.seacroak.basicweapons.item;
 
+import com.seacroak.basicweapons.mixinutils.PlayerEntityAccessor;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ToolComponent;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
 
 public class ClubItem extends BasicWeaponSweeplessItem {
@@ -12,16 +19,15 @@ public class ClubItem extends BasicWeaponSweeplessItem {
   private static ToolComponent createToolComponent() {
     return null;
   }
-//  @Override
-//  public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-//    PlayerEntity player = (PlayerEntity) attacker;
-//    float f2 = ((PlayerEntityAccessor) player).bw$getCooldown(0.5f);
-//    if (f2 > 0.9F) {
-//      target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 15));
-//    }
-//    stack.damage(1, attacker, (e) -> {
-//      e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
-//    });
-//    return true;
-//  }
+
+  @Override
+  public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+    PlayerEntity player = (PlayerEntity) attacker;
+    float f2 = ((PlayerEntityAccessor) player).bw$getCooldown(0.5f);
+    if (f2 > 0.9F) {
+      target.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 15));
+    }
+    stack.damage(1, attacker, EquipmentSlot.MAINHAND);
+    return true;
+  }
 }
--- src/main/java/com/seacroak/basicweapons/item/HammerItem.java
+++ src/main/java/com/seacroak/basicweapons/item/HammerItem.java
@@ -1,8 +1,14 @@
 package com.seacroak.basicweapons.item;
 
+import com.seacroak.basicweapons.mixinutils.PlayerEntityAccessor;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ToolComponent;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.math.Vec3d;
 
 public class HammerItem extends BasicWeaponSweeplessItem {
 
@@ -14,22 +20,20 @@ public class HammerItem extends BasicWeaponSweeplessItem {
     return null;
   }
 
-//  @Override
-//  public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-//    PlayerEntity player = (PlayerEntity) attacker;
-//    float f2 = ((PlayerEntityAccessor) player).bw$getCooldown(0.5f);
-//    if (f2 >= 0.9F) {
-//      if (target.isPlayer()) {
-//        Vec3d currentMovement = target.getVelocity();
-//        target.setVelocity(currentMovement.x, currentMovement.y + 0.8, currentMovement.z);
-//        target.velocityModified = true;
-//      } else {
-//        target.addVelocity(0, 0.45, 0);
-//      }
-//    }
-//    stack.damage(1, attacker, (e) -> {
-//      e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
-//    });
-//    return true;
-//  }
+  @Override
+  public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+    PlayerEntity player = (PlayerEntity) attacker;
+    float f2 = ((PlayerEntityAccessor) player).bw$getCooldown(0.5f);
+    if (f2 >= 0.9F) {
+      if (target.isPlayer()) {
+        Vec3d currentMovement = target.getVelocity();
+        target.setVelocity(currentMovement.x, currentMovement.y + 0.8, currentMovement.z);
+        target.velocityModified = true;
+      } else {
+        target.addVelocity(0, 0.45, 0);
+      }
+    }
+    stack.damage(1, attacker, EquipmentSlot.MAINHAND);
+    return true;
+  }
 }
