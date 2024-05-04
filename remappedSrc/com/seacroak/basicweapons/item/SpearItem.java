90ceef2d91d39cb02142bf325bfa0eea2ddaa24d
Khazoda <khazoda@gmail.com>
2024-05-04 15:22:53 +0100

    remappedSrc cleanup
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- remappedSrc/com/seacroak/basicweapons/item/SpearItem.java
+++
@@ -1,9 +0,0 @@
-package com.seacroak.basicweapons.item;
-
-import net.minecraft.item.ToolMaterial;
-
-public class SpearItem extends BasicWeaponSweeplessItem {
-  public SpearItem(ToolMaterial tier, int damage, float attackSpeed, Settings properties) {
-    super(tier, damage, attackSpeed, properties);
-  }
-}
