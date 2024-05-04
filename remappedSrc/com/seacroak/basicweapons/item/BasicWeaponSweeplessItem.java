498ecca6b89fc910ff221c720b3db912e4b1c35e
Khazoda <khazoda@gmail.com>
2024-05-04 15:23:26 +0100

    remappedSrc cleanup
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- remappedSrc/com/seacroak/basicweapons/item/BasicWeaponSweeplessItem.java
+++
@@ -1,9 +0,0 @@
-package com.seacroak.basicweapons.item;
-
-import net.minecraft.item.ToolMaterial;
-
-public class BasicWeaponSweeplessItem extends BasicWeaponItem {
-  public BasicWeaponSweeplessItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
-    super(toolMaterial, attackDamage, attackSpeed, settings);
-  }
-}
