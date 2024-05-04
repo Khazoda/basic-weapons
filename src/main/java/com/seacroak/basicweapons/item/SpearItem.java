6c4513fbf6e617e08c236c393a25eb1fa35cc6fd
Khazoda <khazoda@gmail.com>
2024-05-04 10:32:41 +0100

    Spears attack an extra 2 blocks away
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/item/SpearItem.java
+++ src/main/java/com/seacroak/basicweapons/item/SpearItem.java
@@ -1,8 +1,14 @@
 package com.seacroak.basicweapons.item;
 
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.AttributeModifierSlot;
+import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.component.type.ToolComponent;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ToolMaterial;
+
+import java.util.UUID;
 
 public class SpearItem extends BasicWeaponSweeplessItem {
   public SpearItem(ToolMaterial tier, Settings settings) {
@@ -11,5 +17,26 @@ public class SpearItem extends BasicWeaponSweeplessItem {
 
   private static ToolComponent createToolComponent() {
     return null;
+  }
+
+  public static AttributeModifiersComponent createAttributeModifiers(ToolMaterial material, int baseAttackDamage, float attackSpeed) {
+    return AttributeModifiersComponent.builder()
+        .add(
+            EntityAttributes.GENERIC_ATTACK_DAMAGE,
+            new EntityAttributeModifier(
+                ATTACK_DAMAGE_MODIFIER_ID, "Weapon modifier", ((float) baseAttackDamage + material.getAttackDamage()), EntityAttributeModifier.Operation.ADD_VALUE
+            ),
+            AttributeModifierSlot.MAINHAND
+        )
+        .add(
+            EntityAttributes.GENERIC_ATTACK_SPEED,
+            new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Weapon modifier", attackSpeed, EntityAttributeModifier.Operation.ADD_VALUE),
+            AttributeModifierSlot.MAINHAND
+        )
+        /* Hopefully this isn't hacky and is in fact safe and correct 😓*/
+        .add(EntityAttributes.PLAYER_ENTITY_INTERACTION_RANGE,
+            new EntityAttributeModifier(UUID.randomUUID(), "Reach modifier", 2, EntityAttributeModifier.Operation.ADD_VALUE),
+            AttributeModifierSlot.MAINHAND)
+        .build();
   }
 }
