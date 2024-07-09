e8893fbbd618729fe8d89fefea902b15a6bb64c4
Khazoda <khazoda@gmail.com>
2024-07-09 17:15:16 +0100

    hammer, dagger value adjustments
    Signed-off-by: Khazoda <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/registry/BWItems.java
+++ src/main/java/com/seacroak/basicweapons/registry/BWItems.java
@@ -10,7 +10,7 @@ import java.util.function.Supplier;
 
 public class BWItems {
   public static final int daggerDamage = 1;
-  public static final float daggerSpeed = -2.0f;
+  public static final float daggerSpeed = -1.6f;
   public static final int hammerDamage = 7;
   public static final float hammerSpeed = -3.4f;
   public static final int clubDamage = 5;
@@ -44,13 +44,13 @@ public class BWItems {
     /* 7 */
     items.add(new ItemInfo("hammer", "stone_hammer", () -> new HammerItem(ToolMaterials.STONE, hammerDamage - 3, hammerSpeed + 0.2f, new Item.Settings())));
     /* 8 */
-    items.add(new ItemInfo("hammer", "iron_hammer", () -> new HammerItem(ToolMaterials.IRON, hammerDamage, hammerSpeed - 0.1f, new Item.Settings())));
+    items.add(new ItemInfo("hammer", "iron_hammer", () -> new HammerItem(ToolMaterials.IRON, hammerDamage, hammerSpeed, new Item.Settings())));
     /* 9 */
-    items.add(new ItemInfo("hammer", "golden_hammer", () -> new HammerItem(ToolMaterials.GOLD, hammerDamage - 3, hammerSpeed, new Item.Settings())));
+    items.add(new ItemInfo("hammer", "golden_hammer", () -> new HammerItem(ToolMaterials.GOLD, hammerDamage - 3, hammerSpeed - 1.0f, new Item.Settings())));
     /* 10 */
-    items.add(new ItemInfo("hammer", "diamond_hammer", () -> new HammerItem(ToolMaterials.DIAMOND, hammerDamage - 1, hammerSpeed, new Item.Settings())));
+    items.add(new ItemInfo("hammer", "diamond_hammer", () -> new HammerItem(ToolMaterials.DIAMOND, hammerDamage - 1, hammerSpeed - 0.1f, new Item.Settings())));
     /* 11 */
-    items.add(new ItemInfo("hammer", "netherite_hammer", () -> new HammerItem(ToolMaterials.NETHERITE, hammerDamage - 1, hammerSpeed, new Item.Settings().fireproof())));
+    items.add(new ItemInfo("hammer", "netherite_hammer", () -> new HammerItem(ToolMaterials.NETHERITE, hammerDamage - 1, hammerSpeed - 0.2f, new Item.Settings().fireproof())));
     /* Batons */
     /* 12 */
     items.add(new ItemInfo("club", "wooden_club", () -> new ClubItem(ToolMaterials.WOOD, clubDamage, clubSpeed, new Item.Settings())));
