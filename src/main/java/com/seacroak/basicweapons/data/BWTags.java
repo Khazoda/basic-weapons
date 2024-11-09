0d7a4b02af8f4dbfe1f7f1ca8afa6e2a7fbbfd81
June <khazoda@gmail.com>
2024-11-09 03:57:47 +0000

    fix sweeping not working on glaive & staff
    Signed-off-by: June <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/data/BWTags.java
+++ src/main/java/com/seacroak/basicweapons/data/BWTags.java
@@ -7,8 +7,9 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class BWTags {
-  public static final TagKey<Item> BASIC_WEAPONS = TagKey.of(RegistryKeys.ITEM, Identifier.of("basicweapons", "enchantable/basic_weapon"));
-  public static final TagKey<Item> NO_SWEEPING = TagKey.of(RegistryKeys.ITEM, Identifier.of("basicweapons", "enchantable/no_sweeping"));
+  public static final TagKey<Item> BASIC_WEAPONS = TagKey.of(RegistryKeys.ITEM, Identifier.of(Constants.BW_NAMESPACE, "enchantable/basic_weapon"));
+  public static final TagKey<Item> HAS_SWEEPING = TagKey.of(RegistryKeys.ITEM, Identifier.of(Constants.BW_NAMESPACE, "enchantable/has_sweeping"));
+  public static final TagKey<Item> NO_SWEEPING = TagKey.of(RegistryKeys.ITEM, Identifier.of(Constants.BW_NAMESPACE, "enchantable/no_sweeping"));
   public static final TagKey<Item> SHARP_WEAPONS = TagKey.of(RegistryKeys.ITEM, Identifier.of(Constants.BW_NAMESPACE, "enchantable/sharp_basic_weapon"));
   public static final TagKey<Item> BLUNT_WEAPONS = TagKey.of(RegistryKeys.ITEM, Identifier.of(Constants.BW_NAMESPACE, "enchantable/blunt_basic_weapon"));
 }
