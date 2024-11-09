1f9c20c6bb5b7071ba791010e2dcd9ead7466d6f
June <khazoda@gmail.com>
2024-11-09 05:11:33 +0000

    fix hammer knockup not working
    Signed-off-by: June <khazoda@gmail.com>

--- src/main/java/com/seacroak/basicweapons/mixin/PlayerEntityMixin.java
+++ src/main/java/com/seacroak/basicweapons/mixin/PlayerEntityMixin.java
@@ -1,10 +1,14 @@
 package com.seacroak.basicweapons.mixin;
 
 import com.seacroak.basicweapons.mixinutils.PlayerEntityAccessor;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin implements PlayerEntityAccessor {
@@ -16,12 +20,13 @@ public abstract class PlayerEntityMixin implements PlayerEntityAccessor {
   @Shadow
   public abstract float getAttackCooldownProgress(float baseTime);
 
-  @Shadow
-  public abstract float getMovementSpeed();
-
   @Override
   public float bw$getCooldown(float baseTime) {
     return this.attackCooldownProgress;
   }
 
+  @Inject(method = "attack", at = @At("HEAD"))
+  private void retrieveCooldownEarly(Entity target, CallbackInfo ci) {
+    attackCooldownProgress = this.getAttackCooldownProgress(0.5f);
+  }
 }
