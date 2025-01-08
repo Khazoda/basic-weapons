88a5f4a00ed8bbc0d1959991e5ec0dcfe18108a4
June <khazoda@gmail.com>
2025-01-08 20:50:42 +0000

    typo

--- common/src/main/java/com/khazoda/basicweapons/mixin/PlayerEntityMixin.java
+++ common/src/main/java/com/khazoda/basicweapons/mixin/PlayerEntityMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Player.class)
 public abstract class PlayerEntityMixin implements PlayerEntityAccessor {
 
-  /*** Pull attack cooldown for use in custom logic based on it, such as hammer swing & club bash ***/
+  /*** Pull attack cooldown for use in custom logic based on it, such as hammer swing and club bash ***/
   @Unique
   protected float basic_weapons$attackCooldownProgress = 0.0f;
 
