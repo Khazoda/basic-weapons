8f36fa63d5cc5f93bd4e65d697c26eac110cc0d2
khazoda <khazoda@gmail.com>
2025-11-16 20:06:45 +0000

    optimized PNGs

--- OptimizePNGAssets.bat
+++ OptimizePNGAssets.bat
@@ -36,21 +36,27 @@ echo:
 set "done=0"
 set "changed=0"
 set "unchanged=0"
+set "savings=0"
+set "original_total=0"
+set "new_total=0"
 for /r "%SEARCH_ROOT%" %%F in (*.png) do (
     set /a done+=1
     set "file=%%~nxF"
 
     :: remember original size
     for %%Z in ("%%F") do set "old=%%~zZ"
+    set /a original_total+=old
 
     "%OXIPNG%" -q -o max --alpha --strip safe -Z "%%F"
     :: compare size
     for %%Z in ("%%F") do (
+        set /a new_total+=%%~zZ
         if "%%~zZ"=="!old!" (
             set /a unchanged+=1
             echo   [%_B%OK 🞑%_X%]  already optimal : !file!
         ) else (
             set /a changed+=1
+            set /a savings+=old - %%~zZ
             echo   [%_G%OK ✔%_X%]  optimised       : !file!
         )
     )
@@ -62,5 +68,12 @@ echo    ────────────────────────
 echo    Finished.  Processed %done% PNG files.
 echo    %_G%✔ %changed% files optimized %_X%
 echo    %_B%🞑 %unchanged% files were already optimized %_X%
+set /a original_kb=original_total/1024
+set /a new_kb=new_total/1024
+set /a savings_kb=savings/1024
+echo    ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─
+echo    Original Total Size: !original_kb! KB
+echo    New Total Size     : !new_kb! KB
+echo    Total space saved: !savings_kb! KB
 echo:
 pause
