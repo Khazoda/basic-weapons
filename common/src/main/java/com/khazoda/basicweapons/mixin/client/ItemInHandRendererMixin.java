38aa869298985dffcbc432ea76f0d60626ec3d95
khazoda <khazoda@gmail.com>
2026-02-10 01:27:57 +0000

    fix locomotion bug properly

--- common/src/main/java/com/khazoda/basicweapons/mixin/client/ItemInHandRendererMixin.java
+++ common/src/main/java/com/khazoda/basicweapons/mixin/client/ItemInHandRendererMixin.java
@@ -4,6 +4,7 @@ import com.khazoda.basicweapons.client.PolearmAnimations;
 import com.khazoda.basicweapons.registry.TagRegistry;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import com.khazoda.basicweapons.platform.Services;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.SubmitNodeCollector;
@@ -32,6 +33,11 @@ public abstract class ItemInHandRendererMixin {
 
   @Inject(method = "renderArmWithItem", at = @At("HEAD"), cancellable = true)
   private void bw$renderPike(AbstractClientPlayer player, float partialTick, float pitch, InteractionHand hand, float swingProgress, ItemStack stack, float equipProgress, PoseStack poseStack, SubmitNodeCollector nodeCollector, int packedLight, CallbackInfo ci) {
+
+    // Let locomotion handle rendering if it's loaded
+    if (Services.PLATFORM.isModLoaded("locomotion")) {
+      return;
+    }
 
     if (bw$isPike(stack)) {
       HumanoidArm humanoidArm = (hand == InteractionHand.MAIN_HAND) ? player.getMainArm() : player.getMainArm().getOpposite();
